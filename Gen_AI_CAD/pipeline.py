b3f561a5d107698be21546d5ef0d9a4e07e116af
SantiagoAO <santiagoao2@hotmail.com>
2025-11-10 11:59:02 -0600

    Added status checks to the pipeline and slightly modified prompts

--- Gen_AI_CAD/pipeline.py
+++ Gen_AI_CAD/pipeline.py
@@ -2,9 +2,12 @@ import subprocess
 
 # Ejecutar el archivo JSONGemini.py
 subprocess.run(["python", "JSONGemini.py"])
+print("JSON file created successfully.")
 
 # Ejecutar el archivo CADGemini.py
 subprocess.run(["python", "CADGemini.py"])
+print("Python file with Cadquery code created successfully.")
 
 # Ejecutar el archivo output.py dónde está el código de CadQuery generado 
 subprocess.run(["python", "output.py"])
+print("CAD file created successfully.")
