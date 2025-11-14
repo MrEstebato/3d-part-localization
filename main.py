fixed normalization

main.py
@@ -8,7 +8,7 @@ import csv
 from preprocessing.cylinders import get_heatstake_centroid
 
 # CONSTANTS
-PATH_TO_STEP_FILE = "doors/doors1.stp"  # Path to the STEP file to be processed
+PATH_TO_STEP_FILE = "doors/doors8.stp"  # Path to the STEP file to be processed
 BOX_SIZE = 11  # mm, length from the centroid of the cylinder to the sides of the box
 
 if __name__ == "__main__":
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     model = GCN3(feature_dim_size=PyG_graphs[0].num_node_features).to(device)
 
     state_dict = torch.load(
-        "GCN/good_heatstake_classifier2.pth", map_location=device, weights_only=False
+        "GCN/heatstake_classifier.pth", map_location=device, weights_only=False
     )
     model.load_state_dict(state_dict)
     model.eval()

preprocessing/graphs.py
@@ -13,12 +13,25 @@ def create_graphs(cylinders):
 
 
 def normalize_features(graphs):
-    all_feats = np.concatenate([g.graph["x"] for g in graphs], axis=0)
-    mean = all_feats.mean(axis=0, keepdims=True)
-    std = all_feats.std(axis=0, keepdims=True) + 1e-6
 
+    i = 0
     for g in graphs:
-        g.graph["x"] = (g.graph["x"] - mean) / std
+        all_feats = np.asarray(g.graph["x"])
+        transposed_feats = all_feats.transpose()
+        normalized = None
+        for i in transposed_feats:
+            # print("i", i)
+            i_mean = i.mean()
+            i_std = i.std() + 1e-6
+            i -= i_mean
+            i /= i_std
+            if normalized is None:
+                normalized = np.array([i])
+            else:
+                normalized = np.concatenate((normalized, [i]), axis=0)
+        normalized = normalized.transpose()
+        # print("Normalized feature shape:", normalized.shape)
+        g.graph["x"] = normalized
 
 
 def get_face_features(face):
@@ -68,7 +81,7 @@ def get_vertex_features(vertex, degree):
     # coordinates (x,y,z)
     coords = np.array(vertex.toTuple(), dtype=np.float32)
 
-    # degree matters (heat stakes ribs attach to cylinder at high valence)
+    # degree
     degree = np.array([degree], dtype=np.float32)
 
     return np.concatenate([coords, degree], axis=0)